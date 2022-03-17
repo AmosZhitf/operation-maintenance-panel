1b046b54aa3b99abbc1c5b78eface99449d2e57d
zhitingfan <916534440@qq.com>
2022-03-17 15:00:48 +0800

    202203171500 新的框架

--- src/store/index.js
+++ src/store/index.js
@@ -1,34 +1,56 @@
-import { createStore } from "vuex";
-import createPersistedState from "vuex-persistedstate";
+import {createStore} from 'vuex'
 
-export const store = createStore({
-  state: {
-    user_group: "",
-    user_id: "",
-    user_token: "",
-    user_metadata: {},
-    permission: [],
-  },
-  mutations: {
-    UPDATE_VARS(state, update_var) {
-      for (const [key, value] of Object.entries(update_var)) {
-        if (key in state) state[key] = value;
-      }
+export default createStore({
+    state: {
+        tagsList: [],
+        collapse: false
     },
-    CLEAR_VARS(state) {
-      let init_state = {
-        user_group: "",
-        user_id: "",
-        user_token: "",
-        user_metadata: {},
-        permission: [],
-      };
-      for (const [key, value] of Object.entries(init_state)) {
-        if (key in state) state[key] = value;
-      }
+    mutations: {
+        delTagsItem(state, data) {
+            state
+                .tagsList
+                .splice(data.index, 1);
+        },
+        setTagsItem(state, data) {
+            state
+                .tagsList
+                .push(data)
+        },
+        clearTags(state) {
+            state.tagsList = []
+        },
+        closeTagsOther(state, data) {
+            state.tagsList = data;
+        },
+        closeCurrentTag(state, data) {
+            for (let i = 0, len = state.tagsList.length; i < len; i++) {
+                const item = state.tagsList[i];
+                if (item.path === data.$route.fullPath) {
+                    if (i < len - 1) {
+                        data
+                            .$router
+                            .push(state.tagsList[i + 1].path);
+                    } else if (i > 0) {
+                        data
+                            .$router
+                            .push(state.tagsList[i - 1].path);
+                    } else {
+                        data
+                            .$router
+                            .push("/");
+                    }
+                    state
+                        .tagsList
+                        .splice(i, 1);
+                    break;
+                }
+            }
+        },
+        // 侧边栏折叠
+        handleCollapse(state, data) {
+            state.collapse = data;
+        }
     },
-  },
-  plugins: [
-    createPersistedState({ storage: window.sessionStorage, key: "ChohoTechOps" }),
-  ],
-});
+    actions: {},
+    modules: {}
+})
